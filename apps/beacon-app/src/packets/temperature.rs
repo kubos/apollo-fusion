95bfc67ceb41e7cb88cb8fd93e46ef996197984d
Catherine Garabedian <catherine@kubos.co>
2019-04-04 14:51:04 +0000

    Cleaning up temperature packet

--- apps/beacon-app/src/packets/temperature.rs
+++ apps/beacon-app/src/packets/temperature.rs
@@ -16,24 +16,39 @@
 
 // Gather all available temperature readings every 15 minutes
 //
-// Message layout:
+// Message layout (28 bytes):
 //  0: EPS motherboard tempurature
 //  1: EPS daughterboard
-//  2: MAI-400 gyroscope
-//  3: MAI-400 RWS motor
-//  4: BIM temperature sensor 1
-//  5: BIM temperature sensor 2
-//  6: BIM temperature sensor 3
-//  7: BIM temperature sensor 4
-//  8: BIM temperature sensor 5
-//  9: BIM temperature sensor 6
-// 10: BM2 internal temperature sensor
-// 11: BM2 external temperature sensor 1 (TS1)
-// 12: BM2 external temperature sensor 2 (TS2)
+//  2: EPS BCR 2 Side A
+//  3: EPS BCR 2 Side B
+//  4: EPS BCR 8 Side A
+//  5: EPS BCR 8 Side B
+//  6: EPS BCR 9 Side A
+//  7: EPS BCR 9 Side B
+//  8: MAI-400 gyroscope
+//  9: MAI-400 RWS motor
+// 10: MAI-400 Detector A Earth Limb Thermistor
+// 11: MAI-400 Detector A Earth Ref Thermistor
+// 12: MAI-400 Detector A Space Ref Thermistor
+// 13: MAI-400 Detector A Wide FOV Thermistor
+// 14: MAI-400 Detector B Earth Limb Thermistor
+// 15: MAI-400 Detector B Earth Ref Thermistor
+// 16: MAI-400 Detector B Space Ref Thermistor
+// 17: MAI-400 Detector B Wide FOV Thermistor
+// 18: BIM temperature sensor 1
+// 19: BIM temperature sensor 2
+// 20: BIM temperature sensor 3
+// 21: BIM temperature sensor 4
+// 22: BIM temperature sensor 5
+// 23: BIM temperature sensor 6
+// 24: BM2 internal temperature sensor
+// 25: BM2 external temperature sensor 1 (TS1)
+// 26: BM2 external temperature sensor 2 (TS2)
+// 27: BM2 temperature range bit field - See section B.30 of the bq34z653 Technical Reference for details
 
 use crate::transmit::*;
 use failure::format_err;
-use kubos_app::query;
+use kubos_app::{query, ServiceConfig};
 use log::*;
 use std::thread;
 use std::time::Duration;
@@ -46,6 +61,42 @@ const EPS_MB_TEMP: &str = r#"{
 
 const EPS_DB_TEMP: &str = r#"{
     telemetry(subsystem: "EPS", parameter: "db_BoardTemperature", limit: 1) {
+        value
+    }
+}"#;
+
+const EPS_BCR2A_TEMP: &str = r#"{
+    telemetry(subsystem: "EPS", parameter: "mb_ArrayTempSa2a", limit: 1) {
+        value
+    }
+}"#;
+
+const EPS_BCR2B_TEMP: &str = r#"{
+    telemetry(subsystem: "EPS", parameter: "mb_ArrayTempSa2b", limit: 1) {
+        value
+    }
+}"#;
+
+const EPS_BCR8A_TEMP: &str = r#"{
+    telemetry(subsystem: "EPS", parameter: "db_ArrayTempSa8a", limit: 1) {
+        value
+    }
+}"#;
+
+const EPS_BCR8B_TEMP: &str = r#"{
+    telemetry(subsystem: "EPS", parameter: "db_ArrayTempSa8b", limit: 1) {
+        value
+    }
+}"#;
+
+const EPS_BCR9A_TEMP: &str = r#"{
+    telemetry(subsystem: "EPS", parameter: "db_ArrayTempSa9a", limit: 1) {
+        value
+    }
+}"#;
+
+const EPS_BCR9B_TEMP: &str = r#"{
+    telemetry(subsystem: "EPS", parameter: "db_ArrayTempSa9b", limit: 1) {
         value
     }
 }"#;
@@ -110,6 +161,15 @@ const MAI_MOTOR_TEMP: &str = r#"{
     }
 }"#;
 
+const BIM_SENSOR_POWER: &str = r#"
+    mutation {
+        passthrough(module: "bim", command: "BIM:TEMP:POW ON") {
+            status,
+            command
+        }
+    }
+"#;
+
 const BIM_TEMP0: &str = r#"{
     telemetry(subsystem: "bim", parameter: "temp0", limit: 1) {
         value
@@ -152,8 +212,6 @@ const BM2_TEMP: &str = r#"{
     }
 }"#;
 
-// TODO: Maybe get/compare the extra BM2 temperature values
-
 const BM2_TS1_TEMP: &str = r#"{
     telemetry(subsystem: "bm2", parameter: "ts1_temp", limit: 1) {
         value
@@ -166,12 +224,30 @@ const BM2_TS2_TEMP: &str = r#"{
     }
 }"#;
 
+const BM2_TEMP_RANGE: &str = r#"{
+    telemetry(subsystem: "bm2", parameter: "temp_range", limit: 1) {
+        value
+    }
+}"#;
+
 pub fn temp_packet(radios: Radios) {
+    // Turn on the BIM's temperature sensors
+    let service = ServiceConfig::new("pumpkin-mcu-service");
+
+    let bim_sensors = query(&service, BIM_SENSOR_POWER, Some(Duration::from_millis(500))).is_ok(); 
+    
     loop {
-        // Operating temp -40 - 100*C, so can be represented by signed byte (i8)
-        // Already converted from raw value
-        let eps_mb_temp: u8 = get_string(&radios, EPS_MB_TEMP).parse::<i8>().unwrap_or(0) as u8;
-        let eps_db_temp: u8 = get_string(&radios, EPS_DB_TEMP).parse::<i8>().unwrap_or(0) as u8;
+        // Float, *C. Operating temp -40 - 100*C, so can be represented by signed byte (i8)
+        // Note: BCR 1, 2, 6, 7, 8, and 9 are connected, but only 2, 8, and 9 have temperature
+        // sensors available
+        let eps_mb_temp: u8 = (get_string(&radios, EPS_MB_TEMP).parse::<f64>().unwrap_or(0.0) as i8) as u8;
+        let eps_db_temp: u8 = (get_string(&radios, EPS_DB_TEMP).parse::<f64>().unwrap_or(0.0) as i8) as u8;
+        let eps_bcr2a_temp: u8 = (get_string(&radios, EPS_BCR2A_TEMP).parse::<f64>().unwrap_or(0.0) as i8) as u8;
+        let eps_bcr2b_temp: u8 = (get_string(&radios, EPS_BCR2B_TEMP).parse::<f64>().unwrap_or(0.0) as i8) as u8;
+        let eps_bcr8a_temp: u8 = (get_string(&radios, EPS_BCR8A_TEMP).parse::<f64>().unwrap_or(0.0) as i8) as u8;
+        let eps_bcr8b_temp: u8 = (get_string(&radios, EPS_BCR8B_TEMP).parse::<f64>().unwrap_or(0.0) as i8) as u8;
+        let eps_bcr9a_temp: u8 = (get_string(&radios, EPS_BCR9A_TEMP).parse::<f64>().unwrap_or(0.0) as i8) as u8;
+        let eps_bcr9b_temp: u8 = (get_string(&radios, EPS_BCR9B_TEMP).parse::<f64>().unwrap_or(0.0) as i8) as u8;
 
         // TODO: Verify that a) we want/care about these values and
         // b) that the conversion equation is correct
@@ -189,30 +265,37 @@ pub fn temp_packet(radios: Radios) {
         // No conversion needed. Raw value is *C, u8
         let mai_gyro_temp: u8 = get_string(&radios, MAI_GYRO_TEMP).parse().unwrap_or(0);
         // Temperature *C = gs_rwsMotorTemp * 0.0402930 - 50
-        // TODO : Figure out max value. Does it exceed 127?
-        let raw: i16 = get_string(&radios, MAI_MOTOR_TEMP).parse().unwrap_or(0);
-        let mai_motor_temp: u8 = (f32::from(raw) * 0.0402930 - 50.0) as u8;
+        let mai_motor_temp: u8 = if let Ok(raw) = get_string(&radios, MAI_MOTOR_TEMP).parse::<i16>() {
+            ((f32::from(raw) * 0.0402930 - 50.0) as i8) as u8
+        } else {
+            0
+        };
 
         // Float, *K. Convert to *C
-        // TODO (somewhere else): The BIM temperature sensors have to be turned on in order to
-        // actually measure the temperature. `BIM:TEMP:POW ON`
-        // TODO: These might be raw values that need to be adjusted with the scalar and offset?
-        // See the supMCU manual...
-        let bim_temp0: u8 =
-            (get_string(&radios, BIM_TEMP0).parse::<f64>().unwrap_or(0.0) - 273.15) as u8;
-        let bim_temp1: u8 =
-            (get_string(&radios, BIM_TEMP1).parse::<f64>().unwrap_or(0.0) - 273.15) as u8;
-        let bim_temp2: u8 =
-            (get_string(&radios, BIM_TEMP2).parse::<f64>().unwrap_or(0.0) - 273.15) as u8;
-        let bim_temp3: u8 =
-            (get_string(&radios, BIM_TEMP3).parse::<f64>().unwrap_or(0.0) - 273.15) as u8;
-        let bim_temp4: u8 =
-            (get_string(&radios, BIM_TEMP4).parse::<f64>().unwrap_or(0.0) - 273.15) as u8;
-        let bim_temp5: u8 =
-            (get_string(&radios, BIM_TEMP5).parse::<f64>().unwrap_or(0.0) - 273.15) as u8;
-
+        // Setting the default values to `273.15` so that the resulting value is zero if we can't
+        // get a good value
+        let (bim_temp0, bim_temp1, bim_temp2, bim_temp3, bim_temp4, bim_temp5) = if bim_sensors {
+            let temp0: u8 = 
+                ((get_string(&radios, BIM_TEMP0).parse::<f64>().unwrap_or(273.15) - 273.15) as i8) as u8;
+            let temp1: u8 =
+                ((get_string(&radios, BIM_TEMP1).parse::<f64>().unwrap_or(273.15) - 273.15) as i8) as u8;
+            let temp2: u8 =
+                ((get_string(&radios, BIM_TEMP2).parse::<f64>().unwrap_or(273.15) - 273.15) as i8) as u8;
+            let temp3: u8 =
+                ((get_string(&radios, BIM_TEMP3).parse::<f64>().unwrap_or(273.15) - 273.15) as i8) as u8;
+            let temp4: u8 =
+                ((get_string(&radios, BIM_TEMP4).parse::<f64>().unwrap_or(273.15) - 273.15) as i8) as u8;
+            let temp5: u8 =
+                ((get_string(&radios, BIM_TEMP5).parse::<f64>().unwrap_or(273.15) - 273.15) as i8) as u8;
+                
+            (temp0, temp1, temp2, temp3, temp4, temp5)
+            
+        } else {
+            (0, 0, 0, 0, 0, 0)
+        };
+        
         // u16, 0.1*K. Convert to whole *C
-        let raw: u16 = get_string(&radios, BM2_TEMP).parse().unwrap_or(0);
+        let raw: u16 = get_string(&radios, BM2_TEMP).parse().unwrap_or(2730);
         let bm2_temp: u8 = (raw / 10 - 273) as u8;
         // i16, 0.1*C. Convert to whole *C
         let raw: i16 = get_string(&radios, BM2_TS1_TEMP).parse().unwrap_or(0);
@@ -220,13 +303,31 @@ pub fn temp_packet(radios: Radios) {
         // i16, 0.1*C. Convert to whole *C
         let raw: i16 = get_string(&radios, BM2_TS2_TEMP).parse().unwrap_or(0);
         let bm2_ts2_temp: u8 = (raw / 10) as u8;
+        // Temperature range bit field
+        let raw: u16 = get_string(&radios, BM2_TEMP_RANGE).parse().unwrap_or(0);
+        // Pulling out only the actual temp range bits
+        let bm2_temp_range = (raw & 0x003F) as u8;
 
         // Turn into data packet
-        let msg: [u8; 13] = [
+        let msg: [u8; 28] = [
             eps_mb_temp,
             eps_db_temp,
+            eps_bcr2a_temp,
+            eps_bcr2b_temp,
+            eps_bcr8a_temp,
+            eps_bcr8b_temp,
+            eps_bcr9a_temp,
+            eps_bcr9b_temp,
             mai_gyro_temp,
             mai_motor_temp,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
             bim_temp0,
             bim_temp1,
             bim_temp2,
@@ -236,6 +337,7 @@ pub fn temp_packet(radios: Radios) {
             bm2_temp,
             bm2_ts1_temp,
             bm2_ts2_temp,
+            bm2_temp_range,
         ];
 
         let _ = radios.transmit(MessageType::Temperature, 0, &msg);
@@ -248,9 +350,9 @@ pub fn temp_packet(radios: Radios) {
 fn get_string(radios: &Radios, msg: &str) -> String {
     match query(&radios.telem_service, msg, Some(Duration::from_millis(100))) {
         Ok(data) => {
-            let value = data["telemetry"][0]["value"].as_str().unwrap_or("0");
+            let value = data["telemetry"][0]["value"].as_str().unwrap_or("");
             value.to_owned()
         }
-        Err(_) => "0".to_owned(),
+        Err(_) => "".to_owned(),
     }
 }
