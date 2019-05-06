a704544927f7e790640d8e8a9ede870686a2353c
Catherine Garabedian <catherine@kubos.co>
2019-05-06 17:11:49 +0000

    Creating temporary NSL package for build purposes

---
+++ external.mk
@@ -0,0 +1,3 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_APOLLO_FUSION_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_APOLLO_FUSION_PATH)/package/*/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_APOLLO_FUSION_PATH)/package/*/*/*/*.mk))
